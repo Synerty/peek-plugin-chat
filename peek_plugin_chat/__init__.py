785fbaf404741ac21fdd25d20ae2651f5fd13deb
Brenton Ford <brenton.ford@synerty.com>
2017-04-04 11:20:43 -0400

    Add Server Service
    ORMOB-205

--- peek_plugin_chat/__init__.py
+++ peek_plugin_chat/__init__.py
@@ -1,1 +1,10 @@
 __version__ = '0.0.1'
+
+
+from peek_plugin_base.server.PluginServerEntryHookABC import PluginServerEntryHookABC
+from typing import Type
+
+
+def peekServerEntryHook() -> Type[PluginServerEntryHookABC]:
+    from ._private.server.ServerEntryHook import ServerEntryHook
+    return ServerEntryHook
