9eb0c4c33af310310d8db977de26021b892a579d
Brenton Ford <brenton.ford@synerty.com>
2017-04-04 12:23:24 -0400

    Add Mobile Service
    ORMOB-205

---
+++ peek_plugin_chat/_private/mobile-app/chat.component.ts
@@ -0,0 +1,14 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: 'plugin-chat',
+    templateUrl: 'chat.component.mweb.html',
+    moduleId: module.id
+})
+export class ChatComponent {
+
+    constructor() {
+
+    }
+
+}
