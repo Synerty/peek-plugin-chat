83441956646d7d5bc0e1581b781adf9188153083
Brenton Ford <brenton.ford@synerty.com>
2017-04-04 11:28:21 -0400

    Add Client Service
    ORMOB-205

--- peek_plugin_chat/__init__.py
+++ peek_plugin_chat/__init__.py
@@ -4,6 +4,13 @@ __version__ = '0.0.1'
 from peek_plugin_base.server.PluginServerEntryHookABC import PluginServerEntryHookABC
 from typing import Type
 
+from peek_plugin_base.client.PluginClientEntryHookABC import PluginClientEntryHookABC
+
+
+def peekClientEntryHook() -> Type[PluginClientEntryHookABC]:
+    from ._private.client.ClientEntryHook import ClientEntryHook
+    return ClientEntryHook
+
 
 def peekServerEntryHook() -> Type[PluginServerEntryHookABC]:
     from ._private.server.ServerEntryHook import ServerEntryHook
