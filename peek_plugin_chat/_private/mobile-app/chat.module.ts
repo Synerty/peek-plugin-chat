50b2f1d33a25089636f0a98b8517529f65ffa4f4
Jarrod Chesney <jarrod.chesney@synerty.com>
2017-08-03 12:52:36 +1000

    Fixed diplicate provides of user services

--- peek_plugin_chat/_private/mobile-app/chat.module.ts
+++ peek_plugin_chat/_private/mobile-app/chat.module.ts
@@ -56,7 +56,6 @@ export function tupleActionPushNameServiceFactory() {
     ],
     exports: [],
     providers: [
-        LoggedInGuard,
         TupleOfflineStorageService,
         {
             provide: TupleOfflineStorageNameService,
