41f84da61dc5374148d029402d15f63d23965e03
Jarrod Chesney <jarrod.chesney@synerty.com>
2019-06-19 14:53:26 +1000

    Updated package publish scripts.
    PEEK-321

--- peek_plugin_chat/__init__.py
+++ peek_plugin_chat/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.3.2'
+__version__ = '0.0.0'
 
 
 from peek_plugin_base.server.PluginServerEntryHookABC import PluginServerEntryHookABC
---
+++ publish.settings.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+PY_PACKAGE="peek_plugin_chat"
+PYPI_PUBLISH="1"
+
+VER_FILES=""
